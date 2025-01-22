eb6c3b3480aff120b11899f549dee2676239e8c5
Tadros Eliott <eliott.tadros@student-cs.fr>
2025-01-22 16:28:40 +0100

    Updated Melio to reflect the latest changes in the code

--- src/control/Controleur.java
+++ src/control/Controleur.java
@@ -16,6 +16,11 @@ public class Controleur implements IControleur {
     @objid ("b0803f31-137b-4f06-b72a-fa3524264004")
     private Entrepot entrepot;
 
+    /**
+     * Méthode appellée par l'IHM quand je joueur appuie sur l'une des
+     * flèches du clavier.
+     * @param direction direction de la progression demandée par le joueur
+     */
     @objid ("0c2325af-a4e2-4e26-90a6-a5f13c58e9ca")
     @Override
     public void action(Direction direction) {
@@ -34,12 +39,21 @@ public class Controleur implements IControleur {
         this.gardien = gardien;
     }
 
+    /**
+     * Méthode appellée par l'IHM pour savoir si le jeu est terminé.
+     * @return true si le jeu est terminé, false sinon
+     */
     @objid ("271eedde-efc8-4a85-87f9-0d15fff02e70")
     @Override
     public boolean jeuTerminé() {
         return this.entrepot.jeuTerminé();
     }
 
+    /**
+     * Méthode appellée par l'IHM pour connaître le nombre de
+     * lignes de l'entrepot courant.
+     * @return le nombre de lignes de l'entrepot
+     */
     @objid ("fc2df654-2108-4fce-8b52-7f73d7bc1c59")
     @Override
     public int getNbLignes() {
@@ -47,6 +61,11 @@ public class Controleur implements IControleur {
         return this.entrepot.getHauteur();
     }
 
+    /**
+     * Méthode appellée par l'IHM pour connaître le nombre de
+     * colonnes de l'entrepot courant.
+     * @return le nombre de colonnes de l'entrepot
+     */
     @objid ("2bcf2581-4fb5-40db-aafd-7b9115d86a6c")
     @Override
     public int getNbColonnes() {
@@ -54,6 +73,13 @@ public class Controleur implements IControleur {
         return this.entrepot.getLargeur();
     }
 
+    /**
+     * Méthode appellée par l'IHM pour connaître le contenu
+     * d'une case de l'entrepot courant.
+     * @param ligne numéro de la ligne de la case (de 0 à getNbLines() - 1)
+     * @param colonne numéro de la colonne de la case (de 0 à getNbColumns() - 1)
+     * @return le type de la case
+     */
     @objid ("a3c8c84c-23c6-406b-bdd0-b6054203b02e")
     @Override
     public ContenuZone getContenu(int ligne, int colonne) {
